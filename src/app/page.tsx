cc8f28227161d3d1e58a59e9206160e415c70bd8
Muhangi2 <eliodamutiba@gmail.com>
2025-01-03 01:07:17 +0300

    add integration and landing page constants; define pricing cards and appointment time slots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import { onGetBlogPosts } from '@/actions/landing'
-import NavBar from '@/components/navbar'
-import { Button } from '@/components/ui/button'
+import NavBar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,12 +8,12 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-// import { pricingCards } from '@/constants/landing-page'
-import clsx from 'clsx'
-import { ArrowRightCircleIcon, Check } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
+} from "@/components/ui/card";
+import { pricingCards } from "@/constants/landing-page";
+import clsx from "clsx";
+import { ArrowRightCircleIcon, Check } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 // import parse from 'html-react-parser'
 // import { getMonthName } from '@/lib/utils'
 
@@ -66,12 +66,12 @@ export default async function Home() {
           {" you're"} not ready to commit you can get started for free.
         </p>
       </section>
-      {/* <div className="flex  justify-center gap-4 flex-wrap mt-6">
+      <div className="flex  justify-center gap-4 flex-wrap mt-6">
         {pricingCards.map((card) => (
           <Card
             key={card.title}
-            className={clsx('w-[300px] flex flex-col justify-between', {
-              'border-2 border-primary': card.title === 'Unlimited',
+            className={clsx("w-[300px] flex flex-col justify-between", {
+              "border-2 border-primary": card.title === "Unlimited",
             })}
           >
             <CardHeader>
@@ -89,10 +89,7 @@ export default async function Home() {
             <CardFooter className="flex flex-col items-start gap-4">
               <div>
                 {card.features.map((feature) => (
-                  <div
-                    key={feature}
-                    className="flex gap-2"
-                  >
+                  <div key={feature} className="flex gap-2">
                     <Check />
                     <p>{feature}</p>
                   </div>
@@ -115,13 +112,10 @@ export default async function Home() {
           Explore our insights on AI, technology, and optimizing your business.
         </p>
       </section>
-      <section className="md:grid-cols-3 grid-cols-1 grid gap-5 container mt-8">
+      {/* <section className="md:grid-cols-3 grid-cols-1 grid gap-5 container mt-8">
         {posts &&
           posts.map((post) => (
-            <Link
-              href={`/blogs/${post.id}`}
-              key={post.id}
-            >
+            <Link href={`/blogs/${post.id}`} key={post.id}>
               <Card className="flex flex-col gap-2 rounded-xl overflow-hidden h-full hover:bg-gray-100">
                 <div className="relative w-full aspect-video">
                   <Image
@@ -132,7 +126,7 @@ export default async function Home() {
                 </div>
                 <div className="py-5 px-10 flex flex-col gap-5">
                   <CardDescription>
-                    {getMonthName(post.createdAt.getMonth())}{' '}
+                    {getMonthName(post.createdAt.getMonth())}{" "}
                     {post.createdAt.getDate()} {post.createdAt.getFullYear()}
                   </CardDescription>
                   <CardTitle>{post.title}</CardTitle>
@@ -143,5 +137,5 @@ export default async function Home() {
           ))}
       </section> */}
     </main>
-  )
+  );
 }
