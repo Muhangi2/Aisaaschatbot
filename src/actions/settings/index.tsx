113d90458470886bbe296af8a661bc66850429cf
Muhangi2 <eliodamutiba@gmail.com>
2025-01-04 17:39:50 +0300

    add InfoBar component and Radix UI Avatar; update settings page structure

---
+++ src/actions/settings/index.tsx
@@ -0,0 +1,347 @@
+"use server";
+import { client } from "@/lib/prisma";
+import { clerkClient, currentUser } from "@clerk/nextjs";
+
+export const onGetSubscriptionPlan = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) return;
+    const plan = await client.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      select: {
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+    if (plan) {
+      return plan.subscription?.plan;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const onGetAllAccountDomains = async () => {
+  const user = await currentUser();
+  if (!user) return;
+  try {
+    const domains = await client.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      select: {
+        id: true,
+        domains: {
+          select: {
+            name: true,
+            icon: true,
+            id: true,
+            customer: {
+              select: {
+                chatRoom: {
+                  select: {
+                    id: true,
+                    live: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    return { ...domains };
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const onUpdatePassword = async (password: string) => {
+  try {
+    const user = await currentUser()
+
+    if (!user) return null
+    const update = await clerkClient.users.updateUser(user.id, { password })
+    if (update) {
+      return { status: 200, message: 'Password updated' }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onGetCurrentDomainInfo = async (domain: string) => {
+  const user = await currentUser()
+  if (!user) return
+  try {
+    const userDomain = await client.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      select: {
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+        domains: {
+          where: {
+            name: {
+              contains: domain,
+            },
+          },
+          select: {
+            id: true,
+            name: true,
+            icon: true,
+            userId: true,
+            products: true,
+            chatBot: {
+              select: {
+                id: true,
+                welcomeMessage: true,
+                icon: true,
+              },
+            },
+          },
+        },
+      },
+    })
+    if (userDomain) {
+      return userDomain
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const onDeleteUserDomain = async (id: string) => {
+  const user = await currentUser()
+
+  if (!user) return
+
+  try {
+    //first verify that domain belongs to user
+    const validUser = await client.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+
+    if (validUser) {
+      //check that domain belongs to this user and delete
+      const deletedDomain = await client.domain.delete({
+        where: {
+          userId: validUser.id,
+          id,
+        },
+        select: {
+          name: true,
+        },
+      })
+
+      if (deletedDomain) {
+        return {
+          status: 200,
+          message: `${deletedDomain.name} was deleted successfully`,
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onCreateHelpDeskQuestion = async (
+  id: string,
+  question: string,
+  answer: string
+) => {
+  try {
+    const helpDeskQuestion = await client.domain.update({
+      where: {
+        id,
+      },
+      data: {
+        helpdesk: {
+          create: {
+            question,
+            answer,
+          },
+        },
+      },
+      include: {
+        helpdesk: {
+          select: {
+            id: true,
+            question: true,
+            answer: true,
+          },
+        },
+      },
+    })
+
+    if (helpDeskQuestion) {
+      return {
+        status: 200,
+        message: 'New help desk question added',
+        questions: helpDeskQuestion.helpdesk,
+      }
+    }
+
+    return {
+      status: 400,
+      message: 'Oops! something went wrong',
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onGetAllHelpDeskQuestions = async (id: string) => {
+  try {
+    const questions = await client.helpDesk.findMany({
+      where: {
+        domainId: id,
+      },
+      select: {
+        question: true,
+        answer: true,
+        id: true,
+      },
+    })
+
+    return {
+      status: 200,
+      message: 'New help desk question added',
+      questions: questions,
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onCreateFilterQuestions = async (id: string, question: string) => {
+  try {
+    const filterQuestion = await client.domain.update({
+      where: {
+        id,
+      },
+      data: {
+        filterQuestions: {
+          create: {
+            question,
+          },
+        },
+      },
+      include: {
+        filterQuestions: {
+          select: {
+            id: true,
+            question: true,
+          },
+        },
+      },
+    })
+
+    if (filterQuestion) {
+      return {
+        status: 200,
+        message: 'Filter question added',
+        questions: filterQuestion.filterQuestions,
+      }
+    }
+    return {
+      status: 400,
+      message: 'Oops! something went wrong',
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onGetAllFilterQuestions = async (id: string) => {
+  try {
+    const questions = await client.filterQuestions.findMany({
+      where: {
+        domainId: id,
+      },
+      select: {
+        question: true,
+        id: true,
+      },
+      orderBy: {
+        question: 'asc',
+      },
+    })
+
+    return {
+      status: 200,
+      message: '',
+      questions: questions,
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onGetPaymentConnected = async () => {
+  try {
+    const user = await currentUser()
+    if (user) {
+      const connected = await client.user.findUnique({
+        where: {
+          clerkId: user.id,
+        },
+        select: {
+          stripeId: true,
+        },
+      })
+      if (connected) {
+        return connected.stripeId
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const onCreateNewDomainProduct = async (
+  id: string,
+  name: string,
+  image: string,
+  price: string
+) => {
+  try {
+    const product = await client.domain.update({
+      where: {
+        id,
+      },
+      data: {
+        products: {
+          create: {
+            name,
+            image,
+            price: parseInt(price),
+          },
+        },
+      },
+    })
+
+    if (product) {
+      return {
+        status: 200,
+        message: 'Product successfully created',
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
