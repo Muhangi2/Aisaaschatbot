a301fed8e883b47db6c2a6fa4ce7b47ff702e060
Muhangi2 <eliodamutiba@gmail.com>
2025-01-15 19:19:13 +0300

    add dark mode toggle and modal component; implement theme selection UI and reusable modal structure

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -2,6 +2,7 @@ import InfoBar from "@/components/infobar";
 // import BillingSettings from '@/components/settings/billing-settings'
 // import ChangePassword from '@/components/settings/change-password'
 // import DarkModetoggle from '@/components/settings/dark-mode'
+// import BillingSettings from "@/components/forms/settings/billings-settings";
 import React from "react";
 
 type Props = {};
@@ -9,7 +10,6 @@ type Props = {};
 const Page = (props: Props) => {
   return (
     <>
-
       <InfoBar />
       <div className="overflow-y-auto w-full chat-window flex-1 h-0 flex flex-col gap-10">
         {/* <BillingSettings /> */}
